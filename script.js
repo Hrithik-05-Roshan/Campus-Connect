5be76304aceb7863669747eb9cb93b4d7401c5f9
Hrithik Burnwal <hrithikrocks124@gmail.com>
2026-01-16 18:16:18 +0530

    Build the Campaigns page : where all the events hosted by the college clubs will listed here.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// locomotive
+
 const scroll = new LocomotiveScroll({
     el: document.querySelector("#main"),
     smooth: true,
     lerp: 0.05
 });
 
+// music play
 const audio = document.getElementById("bgAudio");
 const btn = document.getElementById("audioBtn");
 
@@ -16,3 +19,15 @@ btn.addEventListener("click", () => {
     btn.innerText = "Mute";
   }
 });
+
+// campaigns - search filter
+  const searchInput = document.getElementById("searchInput");
+  const cards = document.querySelectorAll(".campaign-card");
+
+  searchInput.addEventListener("input", () => {
+    const value = searchInput.value.toLowerCase();
+    cards.forEach(card => {
+      const title = card.dataset.title.toLowerCase();
+      card.style.display = title.includes(value) ? "block" : "none";
+    });
+  });
